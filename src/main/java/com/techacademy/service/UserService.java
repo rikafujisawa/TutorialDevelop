cfc985af8a34436b61297ea2746844fa8c77b788
rikafujisawa <k0ma0127@yahoo.co.jp>
2023-03-11 22:45:31 +0900

    lesson17 8

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -23,10 +23,16 @@ public class UserService {
 	}
 
 	//---- 追記：ここから -----
+	/** Userを１件検索して返す */
+	public User getUser(Integer id) {
+		return userRepository.findById(id).get();
+	}
+	// ---- 追加：ここまで　----
+
 	/** Userの登録を行う */
 	@Transactional
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}
-	// ---- 追加：ここまで　----
+
 }

215900c53d8756cd22dcda3d76f46bb971a25bf5
rikafujisawa <k0ma0127@yahoo.co.jp>
2023-03-17 20:46:59 +0900

    Lesson17　課題途中

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -41,7 +41,7 @@ public class UserController {
 		return "user/register";
 	}
 
-	// ----- 変更ここから -----
+
 	/** User登録処理 */
 	@PostMapping("/register")
 	public String postRegister(@Validated User user, BindingResult res, Model model) {
@@ -54,7 +54,7 @@ public class UserController {
 		// 一覧画面にリダイレクト
 		return "redirect:/user/list";
 	}
-	// ----- 変更ここまで -----
+
 
 	/** User更新画面を表示 */
 	@GetMapping("/update/{id}/")
@@ -67,12 +67,19 @@ public class UserController {
 
 	/** User更新処理 */
 	@PostMapping("/update/{id}/")
-	public String postUser(User user) {
+	public String postUser(@Validated User user, BindingResult res, Model model) {
+
+		if(res.hasErrors()) {
+			// エラーあり
+			return getRegister(user);
+		}
 		// User登録
 		service.saveUser(user);
 		// 一覧画面にリダイレクト
 		return "redirect:/user/list";
-	}
+}
+
+
 
 	/** User削除処理 */
 	@PostMapping(path="list", params="deleteRun")
